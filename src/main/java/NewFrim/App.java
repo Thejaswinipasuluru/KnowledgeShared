cc8dfc8f6a1dfec9ac2a45381b0f263251fbe519
unknown <thejaswinipasuluru@gmail.com>
2018-04-15 17:49:26 +0530

    adding mu new project

---
+++ src/main/java/NewFrim/App.java
@@ -0,0 +1,13 @@
+package NewFrim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
